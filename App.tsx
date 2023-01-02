fa93034518a940d569f097a2e88c415042424d0c
myApp <kiril.ebarev@gmail.com>
2023-01-02 19:33:00 +0500

    add tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View} from "react-native";
+import {PageProjects} from "./components/PageProjects";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            {/* <Text>Open up App.tsx to start working on your app!!!!</Text> */}
+            <PageProjects />
+            <StatusBar style='auto' />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ components/PageProjects.tsx
@@ -0,0 +1,45 @@
+/** @format */
+
+import {Text, View} from "react-native";
+// import React, {useState} from "react";
+// import {ButtonAdd} from "../components/ButtonAdd";
+// import {EachProject} from "../components/projects/EachProject";
+// import "../styles/projects.scss";
+// import {AboutMe} from "../components/footer/footer";
+// import {CreateNewProjectModal} from "../components/projects/CreateNewProjectModal";
+// import {useTypedSelector} from "../hooks/useTypedSelector";
+
+export const PageProjects = () => {
+    // const [createNewProjectModal, setCreateNewProjectModal] =
+    //     useState<boolean>(false);
+
+    // const dataProject = useTypedSelector((state) => state.projects);
+
+    return (
+        <View>
+            <View>
+                <Text>Task Board</Text>
+                <View>
+                    <Text>TEST TEMP</Text>
+                    {/* <ButtonAdd
+                        setActive={setCreateNewProjectModal}
+                        text='Create new project'
+                    /> */}
+
+                    {/* <div className='projectsBox'>
+                        {dataProject.map((project) => (
+                            <EachProject project={project} key={project.id} />
+                        ))}
+                    </div> */}
+                </View>
+                {/* <CreateNewProjectModal
+                    active={createNewProjectModal}
+                    setActive={setCreateNewProjectModal}
+                /> */}
+                {/* <>
+                    <AboutMe />
+                </> */}
+            </View>
+        </View>
+    );
+};
